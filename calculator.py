e62d59d8e2a27c8df1d275c95be2ffee66bed8d1
sankalp kumar <65161219+sankalp475@users.noreply.github.com>
2020-10-03 09:53:41 +0530

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,41 @@
+# from functions import*
+
+while True:
+    print("use n fot exit")
+    op = input("choose the operator?'(+ , - , * , /)'\n")
+    # input operator from user
+    if op == 'n':
+        break
+    num1 = float(input("enter first number\n"))
+    # input number 1 from user
+    num2 = float(input("enter the second number\n"))
+    # input number 2 from user
+    
+    def addition(num1,num2):
+        result = num1 + num2
+        print("{0} + {1} = {2}".format(num1,num2,result))
+    def substraction(num1,num2):
+        result = num1 - num2
+        print("{0} - {1} = {2}".format(num1,num2,result))
+    def multiplication(num1,num2):
+        result = num1 * num2
+        print("{0} * {1} = {2}".format(num1,num2,result))
+    def division(num1,num2):
+        if num2 == 0:
+            print("cant divide by zero")
+        else:
+            result = num1 / num2
+            print("{0} / {1} = {2}".format(num1,num2,result))
+            
+    if op == '+':
+        addition(num1,num2)
+    elif op == '-':
+        substraction(num1,num2)
+    elif op == '*':
+        multiplication(num1,num2)   
+    elif op == '/':
+        division(num1,num2)
+    else:
+        print("invalid number")
+    print("thanks for using python calculator")
+    print("\n")
---
+++ functions.py
@@ -0,0 +1,15 @@
+# def addition(num1,num2):
+#     result = num1 + num2
+#     print("{0} + {1} = {2}".format(num1,num2,result))
+# def substraction(num1,num2):
+#     result = num1 - num2
+#     print("{0} - {1} = {2}".format(num1,num2,result))
+# def multiplication(num1,num2):
+#     result = num1 * num2
+#     print("{0} * {1} = {2}".format(num1,num2,result))
+# def division(num1,num2):
+#     if num2 == 0:
+#         print("cant divide by zero")
+#     else:
+#         result = num1 / num2
+#         print("{0} / {1} = {2}".format(num1,num2,result))
