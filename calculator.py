80bad9505aae8d19f167363ebced446e89feedca
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-09-04 17:02:27 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,14 +1,15 @@
+#!/usr/bin/python3
 # from functions import*
 
 while True:
-    print("use n fot exit")
-    op = input("choose the operator?'(+ , - , * , /)'\n")
+    print("use n for exit")
+    op = input("choose the operator?'(+ , - , * , /, %)'\n")
     # input operator from user
     if op == 'n':
         break
-    num1 = float(input("enter first number\n"))
+    num1 = float(input("enter 1st number\n"))
     # input number 1 from user
-    num2 = float(input("enter the second number\n"))
+    num2 = float(input("enter 2nd number\n"))
     # input number 2 from user
     
     def addition(num1,num2):
@@ -26,6 +27,9 @@ while True:
         else:
             result = num1 / num2
             print("{0} / {1} = {2}".format(num1,num2,result))
+    def percentage(num1,num2):
+        result = num1 % num2
+        print("{0} % {1} = {2}". format(num1,num2,result))
             
     if op == '+':
         addition(num1,num2)
@@ -35,6 +39,8 @@ while True:
         multiplication(num1,num2)   
     elif op == '/':
         division(num1,num2)
+    elif op == '%':
+        percentage(num1,num2)
     else:
         print("invalid number")
     print("thanks for using python calculator")
